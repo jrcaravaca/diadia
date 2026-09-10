b272b77039afb1bf1fdd69db06f6f1e13899c8e9
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2026-09-10 04:56:01 +0200

    Implementar routing via API para Alumno y Registro usando Django Rest Frameork

--- backend/registros/urls.py
+++ backend/registros/urls.py
@@ -1,1 +1,13 @@
-from django
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AlumnoViewSet, RegistroViewSet
+
+router = DefaultRouter()
+
+router.register(r'alumnos', AlumnnoViewSet)
+router.register(r'registros', RegistroViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
+
