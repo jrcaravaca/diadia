c5c866f360483c60acbab844cc512790c8db3992
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2026-09-10 04:38:36 +0200

    feat: implement Alumno y Registro ViewSets y configurar urls asociadas

--- backend/registros/views.py
+++ backend/registros/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Alumno, Registro
+from .serializers import AlumnoSerializer, RegistroSerializer
 
-# Create your views here.
+class AlumnoViewSet(viewsets.ModelViewSet): 
+    queryset = Alumno.objects.all()
+    serializer_class = AlumnoSerializer
+
+class RegistroViewSet(viewsets.ModelViewSet): 
+    queryset = Registro.objects.all().order_by('-fecha.hora')
+    serializer_class = RegistroSerializer
+
