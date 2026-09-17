391d9e74cb763d87e556cac8595547d54fb4238e
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2026-09-17 05:27:03 +0200

    fix: refactorizar componente ficha alumno para aplicar nuevos estilos

--- frontend/src/components/FichaAlumno.jsx
+++ frontend/src/components/FichaAlumno.jsx
@@ -15,34 +15,36 @@ export default function FichaAlumno() {
     }, []);
 
     return (
-        <div className="p8 bg-gray-50 min-h-screen">
-            <h1 className="text-3xl font-bold text-gray-800 mb-6">Alumnos</h1>
+        //Fondo y fuente general
+        <div className="p-8 bg-gray-50 min-h-screen font-vietnam text-dia-neutral">
 
             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                 {alumnos.map((alumno) => (
-                    <div key={alumno.id} className="bg-white p-6 rounded-xl shadow-sm border border-gray-100">
-                        <h2 className="text-2xl font-semibold text-blue-600 mb-2">{alumno.nombre}</h2>
-                        <p className="text-sm text-gray-500 mb-4">Aula ID: {alumno.aula}</p>
-
-                        <div className="bgl-blue-50 p-3 rounded-lg">
-                            <h3 className="text-sm font-semibold text-blue-800 mb-2"> Personas Autorizadas:</h3>
-                            {alumno.autorizados.length > 0 ? (
-                                <ul className="text-sm text-gray-700">
-                                    {alumno.autorizados.map(autorizado => (
-                                        <li key={autorizado.id} className="mb-1">
-                                            • {autorizado.nombre_completo} (DNI: {autorizado.dni})
-                                        </li>
-                                    ))}
-                                </ul>
-                            ) : (
-                                <p className="text-sm text-gray-500">Sin autorizados registrados.</p>
-                            )}
+                    // Tarjeta estilo burbuja
+                    <div key={alumno.id} className="bg-white p-6 rounded-4xl shadow-md gap-5 flex flex-col ">
+                        <div className="flex items-center gap-4">
+                            <div className="w-14 h-14 shrink-0 rounded-full bg-dia-secondary flex items-center justify-center font-quicksand font-bold text-xl text-dia-neutral">
+                                {/* Primera letra del nombre */}
+                                {alumno.nombre.charAt(0).toUpperCase()}
+                            </div>
+                            <div className="">
+                                <h2 className="text-xl font-quicksand font-bold tracking-tight">{alumno.nombre}</h2>
+                                <p className="text-sm text-gray-400">Aula ID: {alumno.aula}</p>
+                            </div>
+                        </div>
+                        {/* Personas Autorizadas */}
+                        <div className="p-4 bg-gray-50/80 border border-gray-100 rounded-2xl">
+                            <p className="text-sm font-semibold mb-2">Autorizados:</p>
+                            <div className="flex flex-col gap2">
+                            {alumno.autorizados.map(autorizado => (
+                                <p key={autorizado.id} className="text-sm flex items-center gap-2 text-gray-500">
+                                    <span className="w-1.5 h-1.5 rounded-full bg-dia-primary"></span> {autorizado.nombre_completo}
+                                </p>
+                            ))}
+                            </div>
                         </div>
                     </div>
-                ))
-
-                }
-
+                ))}
             </div>
         </div>
     )
