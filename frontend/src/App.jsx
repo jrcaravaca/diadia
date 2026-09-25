69cc4f7329afe672cc684e8f2a02f885cfccfa74
Jose Ramon <joseramoncaravaca@gmail.com>
2026-09-25 04:40:15 +0200

    feat: integracion de react-router-dom para la navegacion de la app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import FichaAlumno from "./components/FichaAlumno/FichaAlumno.jsx";
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen text-dia-neutral font-vietnam antialiased">
-      <FichaAlumno />
+      <Routes>
+        <Route path="/" element={<Navigate to="/aula/1" />} />
+
+        <Route path="/aula/:id" element={<FichaAlumno />} />
+
+      </Routes>
     </div>
   );
 };
